e41bc9f107b726cbb303afd92b0fa89315aef7ca
Roberto Gianotto <gianottoroberto@gmail.com>
2026-05-22 18:44:01 +0200

    esercizio principainte 2

---
+++ livello-principiante/esercizio_2_P.py
@@ -0,0 +1,19 @@
+# dati 2 numeri interi
+a = 17
+b = 5
+# calcolare:
+somma = a + b
+differenza = a - b
+prodotto = a * b
+divisione = a / b
+divi_intera = a // b
+resto = a % b
+potenza = a ** b
+# stampa a video
+print(f"Somma:{somma}")
+print(f"Differenza:{differenza}")
+print(f"Prodotto:{prodotto}")
+print(f"Divisione:{divisione}")
+print(f"Divisione intera:{divi_intera}")
+print(f"Resto:{resto}")
+print(f"Potenza:{potenza}")
