e2b72b83ebc27596fbea3906c9aa89e40714cb5d
Roberto Gianotto <gianottoroberto@gmail.com>
2026-06-19 22:33:07 +0200

    wip Laboratorio principiante 3

--- livello-principiante/laboratorio_3/progetto_2_P.py
+++ livello-principiante/laboratorio_3/progetto_2_P.py
@@ -31,5 +31,63 @@ domanda_5 = {
 }
 
 
-def crea_domande(lista_domande):
-    pass
+def crea_domande():
+    lista_domande = []
+    lista_domande.append(domanda)
+    lista_domande.append(domanda_2)
+    lista_domande.append(domanda_3)
+    lista_domande.append(domanda_4)
+    lista_domande.append(domanda_5)
+
+    return lista_domande
+
+
+def totale_domande(lista):
+    return len(lista)
+
+
+def mostra_domanda(domanda, numero, totale):
+    # TODO: dobbiamo capire come ricavare il numero corretto della domanda
+    print(f"Domanda {numero}/{totale}: {domanda["testo"]}")
+    for i, opzione in enumerate(domanda["opzioni"]):
+        lettera = chr(i + ord("a"))
+        print(f"  {lettera}) {opzione}")
+
+
+def main():
+    print(50 * "=")
+    print("")
+    print("   QUIZ DI PYTHON - 5 domande")
+    print("")
+    print(50 * "=")
+
+    # qui dentro ho la lista delle 5 domande
+    lista_domande = crea_domande()
+
+    print(lista_domande)
+
+    print(mostra_domanda(domanda_2, 2, totale_domande(lista_domande)))
+
+
+main()
+
+
+"""
+Domanda 1/5: Quale funzione stampa a schermo in Python?
+
+      a) echo()
+
+      b) write()
+
+      c) console.log()
+
+      d) print()
+
+
+
+for i, opzione in enumerate(domanda["opzioni"]):
+    lettera = chr(i + ord("a"))    # 0→'a', 1→'b', 2→'c', 3→'d'
+    print(f"  {lettera}) {opzione}")
+
+
+"""
